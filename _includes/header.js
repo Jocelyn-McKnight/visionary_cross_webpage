6d45dfae3f1e8b2d07d5652c9c7280b7adf06838
Jocelyn McKnight <167371795+Jocelyn-McKnight@users.noreply.github.com>
2024-11-22 13:39:59 -0700

    Update header.js

--- _includes/header.js
+++ _includes/header.js
@@ -1,5 +1,5 @@
 // header.js
-fetch('header.html')
+fetch('_includes/header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header').innerHTML = data;

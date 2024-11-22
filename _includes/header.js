7878a3a168b05c4fce2aba58529b590cdc2a7e3c
Jocelyn McKnight <167371795+Jocelyn-McKnight@users.noreply.github.com>
2024-11-22 13:39:42 -0700

    Create header.js

---
+++ _includes/header.js
@@ -0,0 +1,7 @@
+// header.js
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading header:', error));

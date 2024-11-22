8fdae980833076546b780596be9313e46414e6ee
Jocelyn McKnight <167371795+Jocelyn-McKnight@users.noreply.github.com>
2024-11-22 13:47:50 -0700

    Update header.js

--- _includes/header.js
+++ _includes/header.js
@@ -1,7 +1,14 @@
 // header.js
-fetch('_includes/header.html')
-    .then(response => response.text())
+fetch('header.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
     .then(data => {
         document.getElementById('header').innerHTML = data;
     })
-    .catch(error => console.error('Error loading header:', error));
+    .catch(error => {
+        console.error('Error loading header:', error);
+    });
